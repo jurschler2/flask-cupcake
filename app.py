294db8987f0509b3864a4d9f8fc0121136a723f9
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-03-30 12:32:17 -0700

    lunch

--- app.py
+++ app.py
@@ -1,1 +1,66 @@
 """Flask app for Cupcakes"""
+from flask import Flask, jsonify, request
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+
+def serialize_cupcake(cupcake):
+    """Serialize a cupcake SQLAlchemy obj to dictionary."""
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image
+    }
+
+
+@app.route("/api/cupcakes", methods=["GET"])
+def get_cupcakes():
+    """Get data about all cupcakes"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize_cupcake(c) for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def get_a_cupcake(cupcake_id):
+    """Get data about a single cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = serialize_cupcake(cupcake)
+
+    return jsonify(cupcake=serialized)
+
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_a_cupcake():
+    """Create a cupcake"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"] or None
+
+    new_cupcake = Cupcake(flavor=flavor, size=size,
+                          rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = serialize_cupcake(new_cupcake)
+
+    return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -1,1 +1,38 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """ A Cupcake. """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    flavor = db.Column(db.Text,
+                       nullable=False)
+
+    size = db.Column(db.Text,
+                     nullable=False)
+
+    rating = db.Column(db.Float,
+                       nullable=False)
+
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default="/static/default.png")
